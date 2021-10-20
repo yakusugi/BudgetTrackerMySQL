cd23901eb5b0770fbd69b1b89e23b0278f5c59c5
yakusugi <yosukeito1987@gmail.com>
2021-10-20 20:35:15 +0900

    Oct 20

--- app/src/main/java/com/example/budgettrackermysql/data/BudgetTrackerDao.java
+++ app/src/main/java/com/example/budgettrackermysql/data/BudgetTrackerDao.java
@@ -20,7 +20,7 @@ public class BudgetTrackerDao {
         String insertSQL = "INSERT INTO 2019_records(id, date, storename, productname, type, price) " +
                 "VALUES('" + budgetTracker.getId() + "','" + budgetTracker.getDate() + "'," + budgetTracker.getStoreName() + "'," + budgetTracker.getProductName() + "'," + budgetTracker.getType() + "'," + budgetTracker.getPrice() +")";
 try {
-    con= DriverManager.getConnection("jdbc:mysql://192.168.0.32:3306/budgettracker?characterEncoding=utf8","and_user","android0216");
+    con= DriverManager.getConnection("jdbc:mysql://192.168.0.32:3306/budgettracker?characterEncoding=utf8?connectTimeout=30000&socketTimeout=30000","and_user","android0216");
 //        con= DriverManager.getConnection("jdbc:mysql://192.168.0.32:3306/budgettracker","and_user","android0216");
     smt=con.createStatement();
 //        ResultSet rs=smt.executeQuery("select * from 2019_records");
